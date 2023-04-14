57620623c466b6c4c66c45dbb579f273cd5c539b
Marco Roth <marco.roth@intergga.ch>
2023-04-14 23:57:52 +0200

    Upgrade CableReady to 5.0.0 and StimulusReflex to 3.5.0.rc2

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,10 +10,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 pin "@rails/actioncable", to: "actioncable.esm.js"
 
-pin "cable_ready", to: "https://ga.jspm.io/npm:cable_ready@5.0.0-pre9/dist/cable_ready.min.js"
-pin "stimulus_reflex", to: "https://ga.jspm.io/npm:stimulus_reflex@3.5.0-pre9/dist/stimulus_reflex.min.js"
-pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.6.1/dist/morphdom.js"
+pin "cable_ready", to: "cable_ready.js"
+pin "stimulus_reflex", to: "stimulus_reflex.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/config", under: "config"
-
